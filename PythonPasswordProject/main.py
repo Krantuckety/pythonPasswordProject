f67acd95aaa8d29269e1a36d6665a68e3eafb310
Krantuckety <nferenczhalmy@gmail.com>
2025-09-09 11:08:29 -0500

    Looping!!
    Added functionality for the password generator to generate additional passwords as long as the user wants to

--- PythonPasswordProject/main.py
+++ PythonPasswordProject/main.py
@@ -5,24 +5,39 @@ import random
 from passwordGenerator import generatePassword
 
 def main():
-    print("Welcome to Noah Ferenczhalmys Random Password Generator")
-    # Prompt to ask if numbers should be included
-    userInput = string(input("Do you want numbers in your password? (Y/N)"))             
-    if userInput.lower() == "y":
-        useNumbers = True
-    else:
-        useNumbers = False
-    # Prompt to ask if special characters should be included
-    userInput = String(input("Do you want special characters in your password? (Y/N)"))
-    if userInput.lower() == "y":
-        useSymbols = True
-    else:
-        useSymbols = False
-    length = int(input("Enter password length: "))
-    # Generate password
-    password = generatePassword(length, useNumbers, useSymbols)
-    # Print password
-    print("your randomly generated password is: ", password)
+    # Greeting
+    print("🔑 Welcome to Noah Ferenczhalmys Random Password Generator 🔑")
 
-    if __name__ == "__main__":
-        main()
+    while True:
+        # Prompt for how long password should be
+        length = int(input("Enter password length: "))
+
+        # Prompt for if numbers should be included
+        userInput = (input("Do you want numbers in your password? (Y/N): "))             
+        if userInput.lower() == "y":
+            useNumbers = True
+        else:
+            useNumbers = False
+
+        # Prompt for if special characters should be included
+        userInput = (input("Do you want special characters in your password? (Y/N): "))
+        if userInput.lower() == "y":
+            useSymbols = True
+        else:
+            useSymbols = False
+
+        # Generate password
+        password = generatePassword(length, useNumbers, useSymbols)
+        # Print password
+        print("your randomly generated password is: ", password)
+        
+        # Prompt for if user wants to generate another password
+        repeat = input("\nDo you want to generate another password? (Y/N): ")
+        
+        if repeat.lower() != "y":
+            print("Thanks for trying out my program!!")
+            break # Exit loop
+
+# Call to run the main method
+if __name__ == "__main__":
+    main()
