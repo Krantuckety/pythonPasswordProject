b8ed3f7cd7fa7d42336a447313750fed6e5cdda7
Krantuckety <nferenczhalmy@gmail.com>
2025-12-20 10:10:22 -0600

    Rewrote password strength calculation
    - Changed from a static, if-it-has-it calculation method to an entropy based calculation
    - Rewrote output segment to display strength based on entropy rating
    - Added documentation to calculatePasswordEntropy to explain HOW changes in input affect the entropy rating.

--- PythonPasswordProject/passwordGenerator.py
+++ PythonPasswordProject/passwordGenerator.py
@@ -6,18 +6,10 @@ from collections import Counter
 
 # Function to generate a password
 def generatePassword(length: int, useNumbers: bool, useSymbols: bool, minimizeRepeats: bool):
-    # Calculate constant vars for password generation
-    sizeCharSet = 52  # Initialize to number of letters in the alphabet (uppercase + lowercase)
-    if useNumbers:
-        sizeCharSet += 10  # Add number of digits (0-9)
-    if useSymbols:
-        sizeCharSet += 32  # Add number of common special characters !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-    
-    numPassCombinations = sizeCharSet ** length  # Alt method: math.pow(sizeCharSet, length)
-    
     # Declare variables
     chars = string.ascii_letters
     passwordStrength = 0
+    numPasswordCombinations = 52  # Start with letters only
 
     # If password is long enough, change the number of allowed repeat characters
     match length:
@@ -25,13 +17,10 @@ def generatePassword(length: int, useNumbers: bool, useSymbols: bool, minimizeRe
             maxRepeat = 1   # A character can be repeated 1 time
         case x if x > 16 and x < 22:
             maxRepeat = 2   # A character can be repeated 2 times
-            passwordStrength += 1       # Add 1 point to the passwords Strength
         case x if x > 22 and x < 32:
             maxRepeat = 2   # A character can be repeated 2 times
-            passwordStrength += 3       # Add 2 points to the passwords Strength
         case x if x > 32:
             maxRepeat = 3   # A character can be repeated 3 times
-            passwordStrength += 4       # Add 3 points to the passwords Strength
 
     # Check to include numbers in the password
     if useNumbers:
@@ -39,12 +28,9 @@ def generatePassword(length: int, useNumbers: bool, useSymbols: bool, minimizeRe
     # Check to include special symbols in the password
     if useSymbols:
         chars = chars + string.punctuation
-        if useNumbers:
-            passwordStrength += 1       # Add 1 point to the passwords Strength
     # Check to minimize the number of repeats in the password
     if minimizeRepeats:
         password = generatePasswordNoRepeats(length, chars, maxRepeat)
-        passwordStrength += 1       # Add 1 point to the passwords Strength
         # Output password Strength
         outputPasswordStrength(passwordStrength)
         return password
@@ -53,6 +39,23 @@ def generatePassword(length: int, useNumbers: bool, useSymbols: bool, minimizeRe
     # Output password Strength
     outputPasswordStrength(passwordStrength)
     return password
+
+def calculatePasswordEntropy(numCombinations: int):
+    """
+    Password Entropy Formula: (length) * log2(numCombinations)
+    Below are some example calculations of entropy explaining how different parameters affect the entropy:
+    EX1: 8 character password using only letters (52 characters) = 45.60 bits
+    EX2: 8 character password using letters & numbers (62 characters) = 47.63 bits
+    EX3: 8 character password using letters, numbers & symbols (94 characters) = 52.43 bits
+    # The above demonstrates minuscule gains from nearly doubling the character set size. 
+    EX4: 9 character password using only letters (52 characters) = 51.30 bits
+    EX5: 9 character password using letters, numbers & symbols (94 characters) = 58.99 bits
+    EX6: 13 character password using only letters (94 characters) = 85.21 bits
+    # The above demonstrates significant gains by increasing password length, far greater than achieved by increasing character set size.
+    # Therefore, focus on making passwords longer rather than just more complex.
+    """
+    entropy = math.log2(numCombinations)
+    return entropy
 
 # Function to generate the password with minimized repeat characters
 def generatePasswordNoRepeats(length: int, chars: string, maxRepeat: int):
@@ -73,14 +76,14 @@ def generatePasswordNoRepeats(length: int, chars: string, maxRepeat: int):
     return (''.join(password))
 
 # Function to assess the Strength Rating of a password and tell the user about it
-def outputPasswordStrength(strengthRating: int):
-    # Note: Password length is the greatest feature in making a strong password
-    match strengthRating:
-        case 0 | 1: 
+def outputPasswordStrength(entropyRating: float):
+    # Note: Password length is the greatest contributor to strength rating
+    match entropyRating:
+        case x if (0 <= x < 60): 
             print("\nPassword Strength: WEAK" + "\nTip: Try making your password longer than 16 characters")
-        case 2 | 3:
+        case x if (60 <= x < 80):
             print("\nPassword Strength: MEDIUM")
-        case 4:
+        case x if (80 <= x < 110):
             print("\nPassword Strength: STRONG")
-        case 5 | 6:
+        case x if (110 <= x):
             print("\nPassword Strength: VERY STRONG")
